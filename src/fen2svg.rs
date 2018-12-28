b974cfdbf3e9d9131bec401e0767fcdd8cbfe282
Daniel Lu <daniel@lawrence.lu>
2018-12-27 20:41:10 -0800

    initial commit with code and basic single-threaded server

---
+++ src/fen2svg.rs
@@ -0,0 +1,62 @@
+pub fn fen2svg(fen: String) -> String {
+    let mut svg: String = "".to_string();
+    svg.push_str(SVG_HEAD);
+    svg.push_str(BOARD);
+    let mut y: usize = 0;
+    for row in fen.split("/") {
+        let mut x: usize = 0;
+        for c in row.chars() {
+            match c {
+                'N'|'n'|'K'|'k'|'P'|'p'|'B'|'b'|'R'|'r'|'Q'|'q' =>
+                 svg.push_str(&format!(
+                    r#"<g transform="translate({} {})">{}</g>"#,
+                    x, y, match c {
+                        'N' => W_N,
+                        'n' => B_N,
+                        'R' => W_R,
+                        'r' => B_R,
+                        'K' => W_K,
+                        'k' => B_K,
+                        'Q' => W_Q,
+                        'q' => B_Q,
+                        'P' => W_P,
+                        'p' => B_P,
+                        'B' => W_B,
+                        'b' => B_B,
+                        _ => "",
+                    }
+                )),
+                '2' => x += GRID_SIZE,
+                '3' => x += 2 * GRID_SIZE,
+                '4' => x += 3 * GRID_SIZE,
+                '5' => x += 4 * GRID_SIZE,
+                '6' => x += 5 * GRID_SIZE,
+                '7' => x += 6 * GRID_SIZE,
+                '8' => x += 7 * GRID_SIZE,
+                _ => (),
+            }
+            x += GRID_SIZE;
+        }
+        y += GRID_SIZE;
+    }
+    svg.push_str(SVG_TAIL);
+    svg
+}
+
+const BOARD : &str= r##"<path d="M0 0l45 0l0 360l45 0l0 -360l45 0l0 360l45 0l0 -360l45 0l0 360l45 0l0 -360l45 0l0 360l45 0l0 -45l-360 0l0 -45l360 0l0 -45l-360 0l0 -45l360 0l0 -45l-360 0l0 -45l360 0l0 -45l-360 0l0 -45" fill="#f3f3f3" stroke="none"/>"##;
+const B_N : &str= r##"<path d="M22 10c10.5 1 16.5 8 16 29H15c0-9 10-6.5 8-21" fill="#000"/><path d="M24 18c.38 2.91-5.55 7.37-8 9-3 2-2.82 4.34-5 4-1.042-.94 1.41-3.04 0-3-1 0 .19 1.23-1 2-1 0-4.003 1-4-4 0-2 6-12 6-12s1.89-1.9 2-3.5c-.73-.994-.5-2-.5-3 1-1 3 2.5 3 2.5h2s.78-1.992 2.5-3c1 0 1 3 1 3" fill="#000"/><path d="M9.5 25.5a.5.5 0 1 1-1 0 .5.5 0 1 1 1 0zm5.433-9.75a.5 1.5 30 1 1-.866-.5.5 1.5 30 1 1 .866.5z" fill="#ececec" stroke="#ececec"/><path d="M24.55 10.4l-.45 1.45.5.15c3.15 1 5.65 2.49 7.9 6.75S35.75 29.06 35.25 39l-.05.5h2.25l.05-.5c.5-10.06-.88-16.85-3.25-21.34-2.37-4.49-5.79-6.64-9.19-7.16l-.51-.1z" fill="#ececec" stroke="none"/>"##;
+const B_K : &str= r##"<path d="M22.5 11.63V6" stroke-linejoin="miter"/><path d="M22.5 25s4.5-7.5 3-10.5c0 0-1-2.5-3-2.5s-3 2.5-3 2.5c-1.5 3 3 10.5 3 10.5" fill="#000" stroke-linecap="butt" stroke-linejoin="miter"/><path d="M11.5 37c5.5 3.5 15.5 3.5 21 0v-7s9-4.5 6-10.5c-4-6.5-13.5-3.5-16 4V27v-3.5c-3.5-7.5-13-10.5-16-4-3 6 5 10 5 10V37z" fill="#000"/><path d="M20 8h5" stroke-linejoin="miter"/><path d="M32 29.5s8.5-4 6.03-9.65C34.15 14 25 18 22.5 24.5l.01 2.1-.01-2.1C20 18 9.906 14 6.997 19.85c-2.497 5.65 4.853 9 4.853 9" stroke="#ececec"/><path d="M11.5 30c5.5-3 15.5-3 21 0m-21 3.5c5.5-3 15.5-3 21 0m-21 3.5c5.5-3 15.5-3 21 0" stroke="#ececec"/>"##;
+const B_P : &str = r##"<path d="M22.5 9c-2.21 0-4 1.79-4 4 0 .89.29 1.71.78 2.38C17.33 16.5 16 18.59 16 21c0 2.03.94 3.84 2.41 5.03-3 1.06-7.41 5.55-7.41 13.47h23c0-7.92-4.41-12.41-7.41-13.47 1.47-1.19 2.41-3 2.41-5.03 0-2.41-1.33-4.5-3.28-5.62.49-.67.78-1.49.78-2.38 0-2.21-1.79-4-4-4z" fill="#000"/>"##;
+const B_B : &str = r##"<g fill="#000" stroke-linecap="butt"><path d="M9 36c3.39-.97 10.11.43 13.5-2 3.39 2.43 10.11 1.03 13.5 2 0 0 1.65.54 3 2-.68.97-1.65.99-3 .5-3.39-.97-10.11.46-13.5-1-3.39 1.46-10.11.03-13.5 1-1.354.49-2.323.47-3-.5 1.354-1.94 3-2 3-2z"/><path d="M15 32c2.5 2.5 12.5 2.5 15 0 .5-1.5 0-2 0-2 0-2.5-2.5-4-2.5-4 5.5-1.5 6-11.5-5-15.5-11 4-10.5 14-5 15.5 0 0-2.5 1.5-2.5 4 0 0-.5.5 0 2z"/><path d="M25 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 1 1 5 0z"/></g><path d="M17.5 26h10M15 30h15m-7.5-14.5v5M20 18h5" stroke="#ececec" stroke-linejoin="miter"/>"##;
+const B_Q : &str = r##"<g fill="#000"><g stroke="none"><circle cx="6" cy="12" r="2.75"/><circle cx="14" cy="9" r="2.75"/><circle cx="22.5" cy="8" r="2.75"/><circle cx="31" cy="9" r="2.75"/><circle cx="39" cy="12" r="2.75"/></g><path d="M9 26c8.5-1.5 21-1.5 27 0l2.5-12.5L31 25l-.3-14.1-5.2 13.6-3-14.5-3 14.5-5.2-13.6L14 25 6.5 13.5 9 26z" stroke-linecap="butt"/><path d="M9 26c0 2 1.5 2 2.5 4 1 1.5 1 1 .5 3.5-1.5 1-1.5 2.5-1.5 2.5-1.5 1.5.5 2.5.5 2.5 6.5 1 16.5 1 23 0 0 0 1.5-1 0-2.5 0 0 .5-1.5-1-2.5-.5-2.5-.5-2 .5-3.5 1-2 2.5-2 2.5-4-8.5-1.5-18.5-1.5-27 0z" stroke-linecap="butt"/><path d="M11 38.5a35 35 1 0 0 23 0" fill="none" stroke-linecap="butt"/><path d="M11 29a35 35 1 0 1 23 0m-21.5 2.5h20m-21 3a35 35 1 0 0 22 0m-23 3a35 35 1 0 0 24 0" fill="none" stroke="#ececec"/></g>"##;
+const B_R : &str = r##"<g fill="#000"><path d="M9 39h27v-3H9v3zm3.5-7l1.5-2.5h17l1.5 2.5h-20zm-.5 4v-4h21v4H12z" stroke-linecap="butt"/><path d="M14 29.5v-13h17v13H14z" stroke-linecap="butt" stroke-linejoin="miter"/><path d="M14 16.5L11 14h23l-3 2.5H14zM11 14V9h4v2h5V9h5v2h5V9h4v5H11z" stroke-linecap="butt"/><path d="M12 35.5h21m-20-4h19m-18-2h17m-17-13h17M11 14h23" fill="none" stroke="#ececec" stroke-width="1" stroke-linejoin="miter"/></g>"##;
+const W_N : &str = r##"<path d="M22 10c10.5 1 16.5 8 16 29H15c0-9 10-6.5 8-21" fill="#fff"/><path d="M24 18c.38 2.91-5.55 7.37-8 9-3 2-2.82 4.34-5 4-1.042-.94 1.41-3.04 0-3-1 0 .19 1.23-1 2-1 0-4.003 1-4-4 0-2 6-12 6-12s1.89-1.9 2-3.5c-.73-.994-.5-2-.5-3 1-1 3 2.5 3 2.5h2s.78-1.992 2.5-3c1 0 1 3 1 3" fill="#fff"/><path d="M9.5 25.5a.5.5 0 1 1-1 0 .5.5 0 1 1 1 0zm5.433-9.75a.5 1.5 30 1 1-.866-.5.5 1.5 30 1 1 .866.5z" fill="#000"/>"##;
+const W_K : &str = r##"<path d="M22.5 11.63V6M20 8h5" stroke-linejoin="miter"/><path d="M22.5 25s4.5-7.5 3-10.5c0 0-1-2.5-3-2.5s-3 2.5-3 2.5c-1.5 3 3 10.5 3 10.5" fill="#fff" stroke-linecap="butt" stroke-linejoin="miter"/><path d="M11.5 37c5.5 3.5 15.5 3.5 21 0v-7s9-4.5 6-10.5c-4-6.5-13.5-3.5-16 4V27v-3.5c-3.5-7.5-13-10.5-16-4-3 6 5 10 5 10V37z" fill="#fff"/><path d="M11.5 30c5.5-3 15.5-3 21 0m-21 3.5c5.5-3 15.5-3 21 0m-21 3.5c5.5-3 15.5-3 21 0"/>"##;
+const W_P : &str = r##"<path d="M22.5 9c-2.21 0-4 1.79-4 4 0 .89.29 1.71.78 2.38C17.33 16.5 16 18.59 16 21c0 2.03.94 3.84 2.41 5.03-3 1.06-7.41 5.55-7.41 13.47h23c0-7.92-4.41-12.41-7.41-13.47 1.47-1.19 2.41-3 2.41-5.03 0-2.41-1.33-4.5-3.28-5.62.49-.67.78-1.49.78-2.38 0-2.21-1.79-4-4-4z" fill="#fff"/>"##;
+const W_B : &str = r##"<g fill="#fff" stroke-linecap="butt"><path d="M9 36c3.39-.97 10.11.43 13.5-2 3.39 2.43 10.11 1.03 13.5 2 0 0 1.65.54 3 2-.68.97-1.65.99-3 .5-3.39-.97-10.11.46-13.5-1-3.39 1.46-10.11.03-13.5 1-1.354.49-2.323.47-3-.5 1.354-1.94 3-2 3-2z"/><path d="M15 32c2.5 2.5 12.5 2.5 15 0 .5-1.5 0-2 0-2 0-2.5-2.5-4-2.5-4 5.5-1.5 6-11.5-5-15.5-11 4-10.5 14-5 15.5 0 0-2.5 1.5-2.5 4 0 0-.5.5 0 2z"/><path d="M25 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 1 1 5 0z"/></g><path d="M17.5 26h10M15 30h15m-7.5-14.5v5M20 18h5" stroke-linejoin="miter"/>"##;
+const W_Q : &str = r##"<path d="M8 12a2 2 0 1 1-4 0 2 2 0 1 1 4 0zm16.5-4.5a2 2 0 1 1-4 0 2 2 0 1 1 4 0zM41 12a2 2 0 1 1-4 0 2 2 0 1 1 4 0zM16 8.5a2 2 0 1 1-4 0 2 2 0 1 1 4 0zM33 9a2 2 0 1 1-4 0 2 2 0 1 1 4 0z"/><path d="M9 26c8.5-1.5 21-1.5 27 0l2-12-7 11V11l-5.5 13.5-3-15-3 15-5.5-14V25L7 14l2 12z" stroke-linecap="butt"/><path d="M9 26c0 2 1.5 2 2.5 4 1 1.5 1 1 .5 3.5-1.5 1-1.5 2.5-1.5 2.5-1.5 1.5.5 2.5.5 2.5 6.5 1 16.5 1 23 0 0 0 1.5-1 0-2.5 0 0 .5-1.5-1-2.5-.5-2.5-.5-2 .5-3.5 1-2 2.5-2 2.5-4-8.5-1.5-18.5-1.5-27 0z" stroke-linecap="butt"/><path d="M11.5 30c3.5-1 18.5-1 22 0M12 33.5c6-1 15-1 21 0" fill="none"/>"##;
+const W_R : &str = r##"<g fill="#fff"><path d="M9 39h27v-3H9v3zm3-3v-4h21v4H12zm-1-22V9h4v2h5V9h5v2h5V9h4v5" stroke-linecap="butt"/><path d="M34 14l-3 3H14l-3-3"/><path d="M31 17v12.5H14V17" stroke-linecap="butt" stroke-linejoin="miter"/><path d="M31 29.5l1.5 2.5h-20l1.5-2.5"/><path d="M11 14h23" fill="none" stroke-linejoin="miter"/></g>"##;
+const SVG_HEAD : &str = r##"<svg xmlns="http://www.w3.org/2000/svg" width="360" height="360"><rect width="360" height="360" fill="#75a"/><g fill="none" fill-rule="evenodd" stroke="#000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">"##;
+const SVG_TAIL: &str = r##"</g></svg>"##;
+
+const GRID_SIZE: usize = 45;
---
+++ src/main.rs
@@ -0,0 +1,70 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+extern crate resvg;
+extern crate sha1;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::Read;
+use std::io::Write;
+use std::path::Path;
+use std::fs::File;
+
+mod fen2svg;
+
+fn main() {
+    let _resvg = resvg::init();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    lazy_static! {
+        static ref FEN_SVG_RE: regex::Regex =
+            regex::Regex::new(r"^GET (/(r|n|b|q|k|p|R|N|B|Q|K|P|[1-8])+){8}\.svg HTTP/1.1").unwrap();
+        static ref FEN_PNG_RE: regex::Regex =
+            regex::Regex::new(r"^GET (/(r|n|b|q|k|p|R|N|B|Q|K|P|[1-8])+){8}\.png HTTP/1.1").unwrap();
+    }
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    let req =String::from_utf8_lossy(&buffer[..]);
+    println!("Request: {}", req);
+    let status_200 = "HTTP/1.1 200 OK\r\n\r\n";
+    let status_404 = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+
+    if FEN_SVG_RE.is_match(&req) {
+        let fen = req[5..].split(".svg HTTP/1.1").next().unwrap().to_string();
+
+        stream.write(status_200.as_bytes()).unwrap();
+        stream.write(fen2svg::fen2svg(fen).as_bytes()).unwrap();
+    } else if FEN_PNG_RE.is_match(&req) {
+        let fen = req[5..].split(".png HTTP/1.1").next().unwrap().to_string();
+        let mut filename : String = sha1::Sha1::from(&fen).digest().to_string();
+        filename.push_str(".png");
+        let path = Path::new(&filename);
+        if !path.exists() {
+            let svg = fen2svg::fen2svg(fen);
+            let backend = resvg::default_backend();
+            let opt = resvg::Options::default();
+            let usvg_opt = resvg::usvg::Options::default();
+            let rtree = resvg::usvg::Tree::from_str(svg.as_str(), &usvg_opt).unwrap();
+            let img = backend.render_to_image(&rtree, &opt).unwrap();
+            img.save(path);
+        }
+        let mut png_file = File::open(path).unwrap();
+        let mut png_buffer = Vec::new();
+        png_file.read_to_end(&mut png_buffer);
+        stream.write(status_200.as_bytes()).unwrap();
+        stream.write(png_buffer.as_slice()).unwrap();
+    } else {
+        stream.write(status_404.as_bytes()).unwrap();
+        stream.write("oh no".as_bytes()).unwrap();
+    }
+
+    stream.flush().unwrap();
+}
